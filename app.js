23277bed180202889deed64d8db304a24517c89c
HillaryGil97 <hillarygil97@gmail.com>
2021-01-05 14:05:36 -0600

    terminado

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('./files')
 const operations = require ('./operations')
 console.log('Hola mundo')
 
@@ -6,4 +6,7 @@ const total = operations.sum (2,2)
 const totalRes = operations.resta(5,4)
 console.log(`Total suma: ${total}\nTotal resta: ${totalRes}`)
 
-fs.writeFileSync('./archivo.txt', '')
+fs.escribirSyn('./archivo.txt')
+fs.escribir('./archivo2.txt', 'Si weee :v')
+fs.leer('./archivo2.txt')
+fs.borrar('./archivo.txt')
